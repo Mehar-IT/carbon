b74512bfdc75475081f94f76bcde2697a5611dc6
Mehar-It <hamzatarique2000@gmail.com>
2023-03-05 16:03:16 +0500

    increased req.body size

--- app.js
+++ app.js
@@ -17,9 +17,9 @@ const fileUpload = require("express-fileupload");
 
 app.use(favicon(path.join(__dirname, "public", "icons", "carbon.png")));
 app.use(cors({ credentials: true, origin: true }));
-app.use(express.json());
 app.use(cookieParser());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(fileUpload());
 // app.use(fileUpload({ useTempFiles: true }));
 
